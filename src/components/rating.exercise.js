ddaa6e6afd4af7ce10ba3563d92366c032a3c3fd
Khanh Luu <luukhanh96@gmail.com>
2021-03-08 02:42:24 +0100

    working with react-query

--- src/components/rating.exercise.js
+++ src/components/rating.exercise.js
@@ -2,6 +2,8 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
+import {useMutation, useClient, useQueryClient} from 'react-query'
+import {client} from 'utils/api-client'
 // 🐨 you'll need useMutation and queryCache from react-query
 // 🐨 you'll also need the client from utils/api-client
 import {FaStar} from 'react-icons/fa'
@@ -20,13 +22,24 @@ const visuallyHiddenCSS = {
 
 function Rating({listItem, user}) {
   const [isTabbing, setIsTabbing] = React.useState(false)
+
+  const queryClient = useQueryClient()
+  const updateItem = useMutation(
+    data =>
+      client(`list-items/${listItem.id}`, {
+        method: 'PUT',
+        data,
+        token: user.token,
+      }),
+    {onSettled: () => queryClient.invalidateQueries('list-items')},
+  )
   // 🐨 call useMutation here and call the function "update"
   // the mutate function should call the list-items/:listItemId endpoint with a PUT
   //   and the updates as data. The mutate function will be called with the updates
   //   you can pass as data.
   // 💰 if you want to get the list-items cache updated after this query finishes
   // the use the `onSettled` config option to queryCache.invalidateQueries('list-items')
-  const update = () => {}
+  const update = data => updateItem.mutateAsync(data)
 
   React.useEffect(() => {
     function handleKeyDown(event) {
